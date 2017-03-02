0e95eaa57098f119da6f70739c4922b42b7df335
CRIMX <straybugs@gmail.com>
2017-03-02 16:05:34 +0800

    Added comp editor display photos

--- src/editor/editor.vue
+++ src/editor/editor.vue
@@ -3,6 +3,25 @@
     <div class="row">
       <div class="master-panel col-6">
         <div class="box-container"><master></master></div>
+        <div class="photo-list-container">
+          <div class="photo-list">
+            <div class="masonry-grid-sizer"></div>
+            <div class="masonry-gutter-sizer"></div>
+            <div class="photo-list-item"
+              v-for="photo in photos"
+            >
+              <photo
+                :src="photo.src"
+                :footer="true"
+                :selected="photo.src === $store.state.master.photo ? 'master' :
+                  photo.src === $store.state.twitter.photo ? 'twitter' :
+                  photo.src === $store.state.weibo.photo ? 'weibo' :
+                  false"
+                @clicked="selectPhoto"
+              ></photo>
+            </div>
+          </div>
+        </div>
       </div>
       <div class="slave-panel col-6">
         <div class="box-container"><twitter></twitter></div>
@@ -16,13 +35,43 @@
 <style src="./style.scss"></style>
 
 <script>
+  import Masonry from 'masonry-layout'
+  import imagesLoaded from 'imagesLoaded'
+
+  import * as types from 'src/editor/store/types'
+
   import master from './components/master'
   import twitter from './components/twitter'
   import weibo from './components/weibo'
+  import photo from './components/photo'
 
   export default {
     name: 'app',
+    data () {
+      return {
+        photos: []
+      }
+    },
+    methods: {
+      selectPhoto (payload) {
+        this.$store.commit(types.UPDATE_PHOTO, payload)
+      }
+    },
+    mounted () {
+      let msnry = new Masonry('.photo-list', {
+        itemSelector: '.photo-list-item',
+        columnWidth: '.masonry-grid-sizer',
+        gutter: '.masonry-gutter-sizer',
+        percentPosition: true
+      })
+
+      imagesLoaded('.photo-list').on('progress', () => {
+        // layout Masonry after each image loads
+        msnry.layout()
+      })
+    },
     components: {
+      photo,
       master,
       twitter,
       weibo
@@ -39,7 +88,32 @@
   height: 100%;
 }
 
+#app {
+  padding-top: 20px;
+}
+
 .box-container {
-  height: 40%;
+  height: 45%;
+  margin-bottom: 30px;
+}
+
+.photo-list-container {
+  // display: flex;
+  // justify-content: center;
+}
+
+.photo-list {
+  margin-left: -6px;
+  margin-right: 15px - 6px;
+}
+
+$grid-width: 33%;
+.photo-list-item,
+.masonry-grid-sizer {
+  width: $grid-width;
+}
+
+.masonry-gutter-sizer {
+  width: (100% - $grid-width * 3) / 2;
 }
 </style>
