a5a3b2d1ff4290cf518b32831e3fcb764383387d
CRIMX <straybugs@gmail.com>
2018-12-30 13:22:44 +0800

    add iview

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,14 @@
 import Vue from 'vue'
 import App from './App.vue'
+import iView from 'iview'
+import i18n from 'vue-plugin-webextension-i18n'
+
+import 'iview/dist/styles/iview.css'
 
 Vue.config.productionTip = false
+
+Vue.use(i18n)
+Vue.use(iView)
 
 new Vue({
   render: h => h(App)
